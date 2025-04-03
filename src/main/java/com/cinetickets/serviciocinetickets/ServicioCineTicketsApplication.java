60cc67f303ea13371be35628fafe575bea23dac7
Alexvarela010 <alex.varela01@uceva.edu.co>
2025-04-03 14:27:27 -0500

    Commit inicial

---
+++ src/main/java/com/cinetickets/serviciocinetickets/ServicioCineTicketsApplication.java
@@ -0,0 +1,13 @@
+package com.cinetickets.serviciocinetickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioCineTicketsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicioCineTicketsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cinetickets/serviciocinetickets/ServicioCineTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinetickets.serviciocinetickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioCineTicketsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
