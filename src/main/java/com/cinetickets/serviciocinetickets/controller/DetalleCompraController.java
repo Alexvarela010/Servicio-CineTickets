79931cb2844338bfbfd7a892e3054ab64949c8f3
Alexvarela010 <alex.varela01@uceva.edu.co>
2025-04-06 22:36:20 -0500

    (add): Añadidos endpoints funciones

--- src/main/java/com/cinetickets/serviciocinetickets/controller/DetalleCompraController.java
+++ src/main/java/com/cinetickets/serviciocinetickets/controller/DetalleCompraController.java
@@ -1,7 +1,6 @@
 package com.cinetickets.serviciocinetickets.controller;
 
 import com.cinetickets.serviciocinetickets.entities.Detallecompra;
-import com.cinetickets.serviciocinetickets.entities.Peliculas;
 import com.cinetickets.serviciocinetickets.repository.DetalleCompraRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,29 +18,29 @@ public class DetalleCompraController {
      public DetalleCompraController(DetalleCompraRepository repository) {
          this.repository = repository;
      }
-    @GetMapping("/detallecompras") //Metodo que lista los detalle de compra
+    @GetMapping("/funciones") //Metodo que lista los detalle de compra
     public List<Detallecompra> listarPeliculas() {
         return this.repository.findAll();
     }
 
-    @GetMapping("/detallecompras/{id}") //Metodo que busca un detalle de compra por id
+    @GetMapping("/funciones/{id}") //Metodo que busca un detalle de compra por id
     public Detallecompra buscarPelicula(@PathVariable int id) {
         Optional<Detallecompra> detallecompra=this.repository.findById(id);
         return detallecompra.orElse(null);
     }
 
-    @DeleteMapping("/detallecompras/{id}") //Metodo que elimina un detalle de compra por id
+    @DeleteMapping("/funciones/{id}") //Metodo que elimina un detalle de compra por id
     public void eliminarPelicula(@PathVariable int id) {
         Optional<Detallecompra> detallecompra = this.repository.findById(id);
         detallecompra.ifPresent(detallescompra -> this.repository.delete(detallescompra));
     }
 
-    @PutMapping("/detallecompras") //Metodo que actualiza un detalle de compra
+    @PutMapping("/funciones") //Metodo que actualiza un detalle de compra
     public Detallecompra actualizarPelicula(@RequestBody Detallecompra detallecompra) {
         return this.repository.save(detallecompra);
     }
 
-    @PostMapping("/detallecompras") //Metodo que guarda un detalle de compra
+    @PostMapping("/funciones") //Metodo que guarda un detalle de compra
     public Detallecompra guardarPelicula(@RequestBody Detallecompra detallecompra) {
         return this.repository.save(detallecompra);
     }
--- src/main/java/com/cinetickets/serviciocinetickets/controller/FuncionController.java
+++ src/main/java/com/cinetickets/serviciocinetickets/controller/FuncionController.java
@@ -1,24 +1,38 @@
 package com.cinetickets.serviciocinetickets.controller;
 
+import com.cinetickets.serviciocinetickets.entities.Detallecompra;
 import com.cinetickets.serviciocinetickets.entities.Funcion;
-import com.cinetickets.serviciocinetickets.entities.Peliculas;
 import com.cinetickets.serviciocinetickets.repository.FuncionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/CineTickets-Service")
 public class FuncionController {
-     private FuncionRepository repository;
-     @Autowired
-     public FuncionController(FuncionRepository repository) {
-         this.repository = repository;
-     }
+    private FuncionRepository repository;
+
+    @Autowired
+    public FuncionController(FuncionRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/funciones")
+    public List<Funcion> listarfunciones() {
+        return this.repository.findAll();
+    }
+
+    @GetMapping("/funciones/{id}")
+    public Funcion buscarFuncion(@PathVariable int id) {
+        Optional<Funcion> funcion=this.repository.findById(id);
+        return funcion.orElse(null);
+    }
+
     @PutMapping("/funciones") //Metodo que actualiza una funcion
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public Funcion actualizarFuncion(@RequestBody Funcion funcion) {
@@ -34,7 +48,7 @@ public class FuncionController {
     @DeleteMapping("/funciones/{id}") //Metodo que elimina una funcion por id
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public void eliminarFuncion(@PathVariable int id) {
-         Optional<Funcion> funcion = this.repository.findById(id);
+        Optional<Funcion> funcion = this.repository.findById(id);
         funcion.ifPresent(value -> this.repository.delete(value));
     }
 }
